bc070f44deca52adbe719ae26af4d88daaf0a0a2
Trustin Harris <trustinharris@gmail.com>
2018-06-19 11:40:43 -0400

    Initial Commit

---
+++ Tip Calc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tip Calc
+//
+//  Created by Trustin Harris on 6/19/18.
+//  Copyright © 2018 Trustin Harris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
